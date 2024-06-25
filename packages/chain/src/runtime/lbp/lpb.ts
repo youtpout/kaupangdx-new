a75544b0400be21255c71dfbc7a17544eb0b6021
youtpout <eddyboughioul@hotmail.fr>
2024-06-25 10:38:22 +0200

    check block range and weight

--- packages/chain/src/runtime/lbp/lpb.ts
+++ packages/chain/src/runtime/lbp/lpb.ts
@@ -30,13 +30,21 @@ export const errors = {
   lpTokenSupplyIsZero: () => `LP token supply is zero`,
   amountOutIsInsufficient: () => `Amount out is insufficient`,
   feeCollectorWithAssetAlreadyUsed: () => `Not more than one fee collector per asset id`,
+  InvalidBlockRange: () => `Invalid block range`,
+  MaxSaleDurationExceeded: () => `Duration of the LBP sale should not exceed 2 weeks`,
+  InvalidWeight: () => `Invalid weight`,
+  FeeAmountInvalid: () => `Invalid fee amount`
 };
 
 // we need a placeholder pool value until protokit supports value-less dictonaries or state arrays
 export const placeholderPoolValue = Bool(true);
 
 export const MAX_PATH_LENGTH = 3;
+/// Max weight corresponds to 100%
 export const MAX_WEIGHT: O1UInt64 = O1UInt64.from(100_000_000);
+/// Max sale duration is 14 days
+export const MAX_SALE_DURATION: O1UInt64 = O1UInt64.from(60 * 60 * 24 * 14);
+
 export class TokenIdPath extends Struct({
   path: Provable.Array(TokenId, MAX_PATH_LENGTH),
 }) {
@@ -78,7 +86,13 @@ export class LBP extends RuntimeModule<LBPConfig> {
     return this.feeCollectorWithAsset.get(feeCollectorAssetKey).isSome;
   }
 
-
+  /**
+   * Zero weight at the beginning or at the end of a sale may cause a problem in the price calculation
+   * Minimum allowed weight is 2%. The exponentiation used in the math can overflow when the ration between the weights is higher than 98/2.
+   */
+  public isValidWeight(weight: O1UInt64) {
+    return weight.greaterThanOrEqual(MAX_WEIGHT.div(50)).and(weight.lessThan(MAX_WEIGHT));
+  }
   /**
    * Creates an LBP pool if one doesnt exist yet, and if the creator has
    * sufficient balance to do so.
@@ -112,11 +126,24 @@ export class LBP extends RuntimeModule<LBPConfig> {
     const areTokensDistinct = tokenAId.equals(tokenBId).not();
     const poolDoesNotExist = this.poolExists(poolKey).not();
     const feeCollectorAssetDoesNotExist = this.feeCollectorWithAssetExists(feeCollectorAssetKey).not();
+    const now = this.network.block.height;
+    const nowLessThanStart = now.lessThan(start);
+    const startLessThanEnd = start.lessThan(end);
+    const max2Weeks = end.sub(start).lessThanOrEqual(MAX_SALE_DURATION);
+    const validInitialWeight = this.isValidWeight(initialWeight);
+    const validFinalWeight = this.isValidWeight(finalWeight);
+    const validFeeAmount = fee.fee1.greaterThan(O1UInt64.zero);
 
     // TODO: add check for minimal liquidity in pools
     assert(areTokensDistinct, errors.tokensNotDistinct());
     assert(poolDoesNotExist, errors.poolAlreadyExists());
     assert(feeCollectorAssetDoesNotExist, errors.feeCollectorWithAssetAlreadyUsed());
+    assert(nowLessThanStart, errors.InvalidBlockRange());
+    assert(startLessThanEnd, errors.InvalidBlockRange());
+    assert(max2Weeks, errors.MaxSaleDurationExceeded());
+    assert(validInitialWeight, errors.InvalidWeight());
+    assert(validFinalWeight, errors.InvalidWeight());
+    assert(validFeeAmount, errors.FeeAmountInvalid());
 
     // transfer liquidity from the creator to the pool
     this.balances.transfer(tokenAId, creator, poolKey, tokenAAmount);
